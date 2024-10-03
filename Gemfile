3a2c6d6bf82c797939c0ba9cb19d41366d6e5d0c
mathew newton <mattynewts@gmail.com>
2024-10-03 23:49:07 +0800

    added bootstrap and gems

--- Gemfile
+++ Gemfile
@@ -11,6 +11,11 @@ gem "sprockets-rails"
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
+#simple_form
+gem 'simple_form'
+
+#sassc css
+gem 'sassc-rails'
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'bootstrap';
+import { Tooltip, Popover } from 'bootstrap';
+
+// Enable Bootstrap tooltips and popovers globally
+document.addEventListener('turbolinks:load', () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new Tooltip(tooltipTriggerEl);
+  });
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new Popover(popoverTriggerEl);
+  });
+});
